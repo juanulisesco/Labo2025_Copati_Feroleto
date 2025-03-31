0b3f51581f0c5835647adc5f7a6721e43c97b306
juanulisesco <alumno26.copati.hugue.ulises@ipm.edu.ar>
2025-03-31 12:28:13 -0300

    Se sube carpeta de ejercicios de JAVA

---
+++ Trabajos JAVA/Ejercicio.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ejercicio {
+    public static void main (String [] args){
+        int N = 5;
+        double A = 4.56;
+        char C = 'a';
+        
+    }
+}
