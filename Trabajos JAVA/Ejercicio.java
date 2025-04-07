1ac33e478015acd622c043ed6bf6cec99a1393b3
juanulisesco <alumno26.copati.hugue.ulises@ipm.edu.ar>
2025-04-07 10:32:13 -0300

    Delete Trabajos JAVA/Ejercicio.java

--- Trabajos JAVA/Ejercicio.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Ejercicio {
-    public static void main (String [] args){
-        int N = 5;
-        double A = 4.56;
-        char C = 'a';
-        
-    }
-}
