e80a740dec654221cc7acdb48c4d5b69f306b054
juanulisesco <alumno26.copati.hugue.ulises@ipm.edu.ar>
2025-04-09 09:32:59 -0300

    Update ReemplazoLetra_ej6.java

--- Trabajos JAVA/ReemplazoLetra_ej6.java
+++ Trabajos JAVA/ReemplazoLetra_ej6.java
@@ -1,18 +1,14 @@
 package clase_1;
 import java.util.Scanner;
-public class ReemplazoLetras_ej6 {
+public class ReemplazoLetra_ej6 {
     public static void main(String[] args) {
         Scanner e = new Scanner(System.in);
         int contador= 0;
         String text= "Ayer me compré muñecos de la marca ‘ToyCo’ por internet. ";
-        for (int i = 0; i < text.length(); i++ ){
-            if(text.charAt(i) == 'e' || text.charAt(i) == 'E' || text.charAt(i) == 'é' || text.charAt(i) == 'É'){
-                System.out.println("Ingrese una letra o numero porla cual quiere reemplazar: ");
-                String rem= e.nextLine();
-                text.charAt(i)= rem;
-
-            }
-        }
+        System.out.println("Ingrese la letra por la cual quiere cambiar a las e: ");
+        char letra= e.next().charAt(0);
+        text.replace('e', letra );
         System.out.println(contador);
+        System.out.println(text);
     }
 }
